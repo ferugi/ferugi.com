ced4d2237d3959fea6874db00fe97ac69ad65f39
ferugi <me@ferugi.com>
2017-01-22 00:13:35 +0000

    Added top & bottom bars & standardized colours.

--- themes/ruji/source/js/site.js
+++ themes/ruji/source/js/site.js
@@ -1,23 +1,3 @@
-/*
-var $left = $('.left-side');
-var $right = $('.right-side');
-
-function init () {
-    oldTransition = $left.css("transition");
-    oldWidth = $left.css("width");
-}
-
-$(document).ready(init);
-
 $(document).scroll(function(){
-
-    if ($left.offset().top == 0) {
-        $left.css("width", "50%");
-        $right.css("margin-left", "50%");
-    } else {
-        $left.css("width", "15em");
-        $right.css("margin-left", "15em");
-    }
-
+    console.log("lol");
 });
-*/
