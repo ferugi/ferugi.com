f688236300ce26a91d337daff99c2073b47dff85
Ferugi El Heri <me@ferugi.com>
2021-01-24 20:10:56 +0000

    Updated fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 // tailwind.config.js
 module.exports = {
+    theme: {
+      fontFamily: {
+      'display': ['EB Garamond', 'serif'],
+      'display-mono': ['PT Mono', 'monospace'],
+      'mono': ['PT Mono', 'monospace'],
+      'body': ['Montserrat', 'sans-serif']
+       }
+    },
     purge: [
       // Use *.tsx if using TypeScript
       './pages/**/*.tsx',
