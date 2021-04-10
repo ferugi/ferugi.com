fc40b789da675acb8398c16aa0dc2f759be7476c
Ferugi El Heri <me@ferugi.com>
2021-04-10 02:24:47 +0100

    Two attempts to get playwright-core working

--- pages/api/seo-image.tsx
+++ pages/api/seo-image.tsx
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as playwright from 'playwright-aws-lambda'
 import ReactDOMServer from 'react-dom/server'
+import * as browsers from 'playwright-core/browsers.json'
 
 type ImageType = 'facebook' | 'twitter'
 
@@ -21,6 +22,11 @@ const ImageProportions: {[k in ImageType]: {
 const TestElement = () => <div>HELLO WORLD</div>
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    if (!browsers) {
+      throw new Error('browsers.json not defined')
+    }
+
     // Process incoming request
     // Get the type the request is for
     // If its a blog post, get the post ID
